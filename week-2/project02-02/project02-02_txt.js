cde8feee1993ca5c6538298686a1df0fed4c4be7
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-01-17 09:44:55 -0700

    Update project02-02

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,20 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Diana Ruiz Garcia
+      Date: 01/17/2024
 
       Filename: project02-02.js
  */
  
+// set the verify form's values
+function verifyForm() {
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+
+      name && email && phone ? window.alert("Thank you!") : window.alert("Please fill in all fields");
+}
+
+// set alert when submit button is clicked
+document.getElementById("submit").addEventListener("click", verifyForm);
