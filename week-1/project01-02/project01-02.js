60252ae975de19f6c64a9542ac6525c26770d5b4
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-03-06 17:44:32 -0700

    Update on project01-02

--- week-1/project01-02/project01-02.js
+++ week-1/project01-02/project01-02.js
@@ -1,20 +1,21 @@
-/*  JavaScript 7th Edition
-    Chapter 1
-    Hands-On Project 1-2
+/*  
+  JavaScript 7th Edition
+  Chapter 1
+  Hands-On Project 1-2
 
-    Author: Diana Ruiz Garcia
-    Date: 01/12/2024 
+  Author: Diana Ruiz Garcia
+  Date: 01/12/2024 
 
-    Filename: project01-02.js
+  Filename: project01-02.js
 */
 
-//define variables for service name and service speed
-var service1Name = "Basic";
-var service2Name = "Express";
-var service3Name = "Extreme";
-var service4Name = "Ultimate"; 
+// Define variables for service name and service speed
+const service1Name = "Basic";
+const service2Name = "Express";
+const service3Name = "Extreme";
+const service4Name = "Ultimate"; 
 
-var service1Speed = "50 Mbps"; 
-var service2Speed = "100 Mbps";
-var service3Speed = "500 Mbps";
-var service4Speed = "1 Gig";
+const service1Speed = "50 Mbps"; 
+const service2Speed = "100 Mbps";
+const service3Speed = "500 Mbps";
+const service4Speed = "1 Gig";
