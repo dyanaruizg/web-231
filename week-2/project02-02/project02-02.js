401948024ef300d7dc254cdddcc160a084a05c45
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-03-06 17:39:02 -0700

    Update on project02-02

--- week-2/project02-02/project02-02.js
+++ week-2/project02-02/project02-02.js
@@ -1,22 +1,23 @@
-/*    JavaScript 7th Edition
-      Chapter 2
-      Project 02-02
+/*    
+  JavaScript 7th Edition
+  Chapter 2
+  Project 02-02
 
-      Application to test for completed form
-      Author: Diana Ruiz Garcia
-      Date: 01/17/2024
+  Application to test for completed form
+  Author: Diana Ruiz Garcia
+  Date: 01/17/2024
 
-      Filename: project02-02.js
+  Filename: project02-02.js
  */
  
-// set the verify form's values
+// Function that verify form's values
 function verifyForm() {
-      let name = document.getElementById("name").value;
-      let email = document.getElementById("email").value;
-      let phone = document.getElementById("phone").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
 
-      name && email && phone ? window.alert("Thank you!") : window.alert("Please fill in all fields");
+  name && email && phone ? window.alert("Thank you!") : window.alert("Please fill in all fields");
 }
 
-// set alert when submit button is clicked
+// Set alert when submit button is clicked
 document.getElementById("submit").addEventListener("click", verifyForm);
