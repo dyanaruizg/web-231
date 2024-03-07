02bb2abd59a84bd4ebdd9641c71a04e169ff78eb
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-03-06 17:06:20 -0700

    Update on project05-01

--- week-7/project05-01/project05-01.js
+++ week-7/project05-01/project05-01.js
@@ -1,18 +1,19 @@
 "use strict";
-/*    JavaScript 7th Edition
-      Chapter 5
-      Project 05-01
+/*    
+  JavaScript 7th Edition
+  Chapter 5
+  Project 05-01
 
-      Project to present an online quiz with a countdown clock
-      Author: Diana Ruiz Garcia
-      Date: 02/22/2024
+  Project to present an online quiz with a countdown clock
+  Author: Diana Ruiz Garcia
+  Date: 02/22/2024
 
-      Filename: project05-01.js
+  Filename: project05-01.js
 */
 
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
-const quizTime = 90;
+const quizTime = 20;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
 // Elements in the quiz page
@@ -51,9 +52,7 @@ function countdown() {
       // If that condition is false, the quiz catches your mistakes 
       // and reports the number of correct answers out of the total number of questions
       window.alert("Incorrect answers: " + totalCorrect + " - Total questions: " + correctAnswers.length);
-      timeLeft = quizTime; // Resets the time left to the quiz time
-      quizClock.value = timeLeft; // Set the quiz clock value to the time left
-      overlay.className = "hidequiz"; // Hide the quiz
+      window.setTimeout(hideQuiz, 5000); // Set time to see the incorrect answers highlighted
     }
   } else {
     // If the condition is false, the clock countdowns the time
@@ -62,17 +61,24 @@ function countdown() {
   }
 }
 
+// Function to hide the quiz
+function hideQuiz() {
+  timeLeft = quizTime; // Resets the time left to the quiz time
+  quizClock.value = timeLeft; // Set the quiz clock value to the time left
+  overlay.className = "hidequiz"; // Hide the quiz
+}
+
 /*------------- Function to check the student answers ----------------*/
 function checkAnswers() {
-   let correctCount = 0;
-   
-   for (let i = 0; i < questionList.length; i++) {
-      if (questionList[i].value === correctAnswers[i]) {
-         correctCount++;
-         questionList[i].className = "";
-      } else {
-         questionList[i].className = "wronganswer";
-      }      
-   }
-   return correctCount;
+  let correctCount = 0;
+  
+  for (let i = 0; i < questionList.length; i++) {
+    if (questionList[i].value === correctAnswers[i]) {
+      correctCount++;
+      questionList[i].className = "";
+    } else {
+      questionList[i].className = "wronganswer";
+    }      
+  }
+  return correctCount;
 }
